1c1c502ad5c3c0551e374facd7c66761191be138
Soldy <4786022+Soldy@users.noreply.github.com>
2020-12-18 22:40:07 +0000

    multi stor preparation
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ exports.temprc=function(storageFD, settings, indexes){
      * @public
      * @return {boolean}
      */
-    this.save = function(){
-        return manager.save();
+    this.save = function(stor){
+        stor = storCheck(stor);
+        return manager[stor].save();
     };
     /*
      * @public
@@ -23,38 +24,43 @@ exports.temprc=function(storageFD, settings, indexes){
      * @public
      * @return {boolean}
      */
-    this.indexRefresh = function(){
-        return manager.indexRefresh();
+    this.indexRefresh = function(stor){
+        stor = storCheck(stor);
+        return manager[stor].indexRefresh();
     };
     /*
      * @param {string} id
      * @public
      * @return {mixed}
      */
-    this.get=function(id){
-        return manager.get();
+    this.get=function(id, stor){
+        stor = storCheck(stor);
+        return manager[stor].get();
     };
     /*
      * @public
      * @return {string}
      */
-    this.hash=function(){
-        return manager.hash();
+    this.hash=function(stor){
+        stor = storCheck(stor);
+        return manager[stor].hash();
     };
     /*
      * @public
      * @return {string}
      */
-    this.hashCheck=function(){
-        return manager.hashCheck();
+    this.hashCheck=function(stor){
+        stor = storCheck(stor);
+        return manager[stor].hashCheck();
     };
     /*
      * @param {string} id
      * @public
      * @return {boolean}
      */
-    this.del=function(id){
-        return manager.del();
+    this.del=function(id, stor){
+        stor = storCheck(stor);
+        return manager[stor].del();
     };
     /*
      * @param {string} id
@@ -62,69 +68,78 @@ exports.temprc=function(storageFD, settings, indexes){
      * @public
      * @return  {boolean}
      */
-    this.set=function(id, val){
-        return manager.set();
+    this.set=function(id, val, stor){
+        stor = storCheck(stor);
+        return manager[stor].set();
     };
     /*
      * @param {string}- id
      * @public
      * @return {boolean}
      */
-    this.check=function(id){
-        manager.check();
+    this.check=function(id, stor){
+        stor = storCheck(stor);
+        manager[stor].check();
     };
     /*
      * @public
      * @return {object}
      */
-    this.full=function(){
-        manager.full();
+    this.full=function(stor){
+        stor = storCheck(stor);
+        manager[stor].full();
     };
     /*
      * @public
      * @return {object}
      */
-    this.all=function(){
-        manager.all();
+    this.all=function(stor){
+        stor = storCheck(stor);
+        manager[stor].all();
     };
     /*
      *
      * @public
      * @return {array}
      */
-    this.list=function(){
-        manager.list();
+    this.list=function(stor){
+        stor = storCheck(stor);
+        manager[stor].list();
     };
     /*
      * @public
      * @return integer
      */
-    this.size=function(){
-        return manager.size();
+    this.size=function(stor){
+        stor = storCheck(stor);
+        return manager[stor].size();
     };
     /*
      * @public
      * @return {boolean}
      */
-    this.empty=function(){
-        manager.empty();
+    this.empty=function(stor){
+        stor = storCheck(stor);
+        manager[stor].empty();
     };
     /*
      * @public
      * @return {object}
      */
-    this.stats=function(){
-        manager.stats();
+    this.stats=function(stor){
+        stor = storCheck(stor);
+        manager[stor].stats();
     };
     /*
      * @param {object}
      * @public
      * @return {boolean}
      */
-    this.importing = function(importDb){
-        manager.importing();
-    }
-    let manager;
+    this.importing = function(importDb, stor){
+        stor = storCheck(stor);
+        manager[stor].importing();
+    };
+    let manager = {};
     /*
      * setup  helper
      * @private
@@ -157,25 +172,37 @@ exports.temprc=function(storageFD, settings, indexes){
         }
     });
     /*
+     * @param {string}
+     *@private
+     */
+    const storCheck = function(stor){
+        if(typeof stor === 'undefined')
+            return 'default';
+        return stor;
+    };
+    /*
      *@private
      */
     const init = function(){
-         if(setup.get('databbaseType') === 'multi'){
-             manager = new (require('./multifile')).temprc(
-                 storageFD,
-                 setup,
-                 indexes
-             );
-             return true;
-         }
-         manager = new (require('./singlefile')).temprc(
-             storageFD,
-             setup,
-             indexes
-         );
-    }
+        if(typeof storageFD === 'string'){
+            if(setup.get('databbaseType') === 'multi'){
+                manager['default'] = new (require('./multifile')).temprc(
+                    storageFD,
+                    setup,
+                    indexes
+                );
+                return true;
+            }
+            manager['default'] = new (require('./singlefile')).temprc(
+                storageFD,
+                setup,
+                indexes
+            );
+            return true;
+        }
+    };
     if(typeof settings !== 'undefined')
         setup.setup(settings);
-
-}
+    init();
+};
 
